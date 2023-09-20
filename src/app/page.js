f709b062ab2db96b74e40576fb188e08c6aa40e9
Xavier <xavierguertin@gmail.com>
2023-09-19 22:11:36 -0400

    Index Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,76 @@
 import Image from 'next/image'
+import styles from "../styles/home.module.css";
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className={styles.container}>
+                <header className="{styles.header} bg-gray-200 p-4 rounded-md shadow-md"><Image src="/image/logo.jpg"
+                                                                                                alt="Logo" width={300}
+                                                                                                height={50}/>
+                    <nav className="flex justify-between items-center">
+                        <ul className="flex space-x-4">
+                            <li className="hover:bg-gray-300 p-2 rounded transition duration-300 ease-in-out"><Link
+                                href="#">Home</Link></li>
+                            <li className="hover:bg-gray-300 p-2 rounded transition duration-300 ease-in-out"><Link
+                                href="#">Services</Link></li>
+                            <li className="hover:bg-gray-300 p-2 rounded transition duration-300 ease-in-out"><Link
+                                href="#">Pricing</Link></li>
+                            <li className="hover:bg-gray-300 p-2 rounded transition duration-300 ease-in-out"><Link
+                                href="#">Contact</Link></li>
+                        </ul>
+                    </nav>
+                    <h1>Welcome to Watch&Ship!</h1>
+                    <p>Deliver packages with ease and save money by watching ads.</p>
+                </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                <section className="{styles.section} bg-white p-4 my-4 rounded-md shadow-md">
+                    <h2>How It Works</h2>
+                    <p>Request a quote for package delivery. If you want to save on the quote, simply watch ads and get
+                        discounts!</p>
+                </section>
+                <section className="{styles.section} bg-white p-4 my-4 rounded-md shadow-md">
+                    <h2>Testimonials</h2>
+                    <div className="testimonial">
+                        <p>"This service is amazing!" - John Doe</p></div>
+                    <div className="testimonial"><p>"Saved a lot on shipping!" - Jane Smith</p></div>
+                </section>
+                <section className="{styles.section} bg-white p-4 my-4 rounded-md shadow-md"><h2>FAQ</h2>
+                    <div className="faq-item"><h3>How does it work?</h3><p>We provide discounts when you watch ads.</p>
+                    </div>
+                    <div className="faq-item"><h3>How much can I save?</h3><p>Discounts vary based on the number of ads
+                        you watch.</p></div>
+                </section>
+                <section className="{styles.section} bg-white p-4 my-4 rounded-md shadow-md"><h2>Features</h2>
+                    <div className="feature-card"><Image src="/image/quickDelivery.png" width={300} height={50}
+                                                         alt="Feature 1"/><p>Quick
+                        Delivery</p></div>
+                    <div className="feature-card"><Image src="/image/wideShipping.png" width={300} height={50}
+                                                         alt="Feature 2"/><p>Wide
+                        Coverage</p></div>
+                </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <section className="{styles.section} bg-white p-4 my-4 rounded-md shadow-md">
+                    <Link href="/quote">
+                        <button
+                            className="bg-blue-600 hover:bg-blue-700 text-white p-2 rounded transition duration-300 ease-in-out">Get
+                            a Quote
+                        </button>
+                    </Link>
+                </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                <footer className="{styles.footer} bg-gray-200 p-4 mt-4 rounded-md shadow-md">
+                    <div className="social-media-icons">
+                        <Link href="#"><Image src="/image/facebook.png" alt="Facebook" width={30} height={30}/></Link>
+                        <Link href="#"><Image src="/image/twitter.png" alt="Twitter" width={30} height={30}/></Link>
+                        <Link href="#"><Image src="/image/instagram.png" alt="Instagram" width={30} height={30}/></Link>
+                    </div>
+                    <Link href="#">Contact</Link>
+                    <Link href="#">About Us</Link>
+                    <Link href="#">Terms of Service</Link>
+                </footer>
+            </div>
+        </main>
+    )
 }
