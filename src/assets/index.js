c24eb364478e3c8d45f346485b35e461a3a47005
Xavier <xavierguertin@gmail.com>
2023-09-28 19:03:49 -0400

    Remove unnecessary import

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,6 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 import google from "./google.svg";
 import apple from "./apple.svg";
-import arrowUp from "./arrow-up.svg";
 import discount from "./Discount.svg";
 import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
@@ -22,7 +21,6 @@ export {
     close,
     google,
     apple,
-    arrowUp,
     discount,
     facebook,
     instagram,
