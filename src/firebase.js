5e98a3d3efb056dda2b0e69a2e8b6e879b3de922
Xavier Guertin <xavierguertin@gmail.com>
2023-10-02 21:39:38 -0400

    Added Firebase dependencies and config

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDzcNuvfEqPQEurSNTgJtDn2WNE0iZDzfc",
+    authDomain: "watchandship.firebaseapp.com",
+    projectId: "watchandship",
+    storageBucket: "watchandship.appspot.com",
+    messagingSenderId: "643698374126",
+    appId: "1:643698374126:web:a65a0b83ea604037ffcaff",
+    measurementId: "G-PV4CC4SR5Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
