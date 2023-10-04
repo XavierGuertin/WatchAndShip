0f92d93aae307223ecaf4be124e4d46c85ae0c79
Xavier Guertin <xavierguertin@gmail.com>
2023-10-04 14:09:39 -0400

    Fixed CSS Issue After Conversion

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,29 @@
 import styles from "../styles/style";
-import {Tracking, GetAQuote, Footer, Navbar, Numbers, MainSection, Features} from "../components";
+import {Features, Footer, GetAQuote, MainSection, Navbar, Numbers, Tracking} from "../components";
 
 const Page = () => (
     <div className="bg-primary w-full overflow-hidden">
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Navbar/>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+                <Navbar/>
+            </div>
         </div>
-      </div>
 
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <MainSection/>
+        <div className={`bg-primary ${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+                <MainSection/>
+            </div>
         </div>
-      </div>
 
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Features/>
-          <Tracking/>
-          <GetAQuote/>
-          <Numbers/>
-          <Footer/>
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+                <Features/>
+                <Tracking/>
+                <GetAQuote/>
+                <Numbers/>
+                <Footer/>
+            </div>
         </div>
-      </div>
     </div>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
       },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
-}
+};
