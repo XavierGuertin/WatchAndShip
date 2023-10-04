019dcf17f2d00ecc73bcb77a204e4225c5f4252a
Xavier Guertin <xavierguertin@gmail.com>
2023-10-04 14:09:14 -0400

    Modify layout to add title and icon to <head>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,16 @@
 import './index.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html>
+          <head>
+              <meta charSet="UTF-8"/>
+              <link rel="icon" type="image/svg+xml" href="/watch&ship.svg"/>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+              <meta name="theme-color" content="#000000"/>
+              <title>Watch & Ship</title>
+          </head>
+          <body>{children}</body>
+      </html>
   )
 }
